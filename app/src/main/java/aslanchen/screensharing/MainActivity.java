e38221ee2c450302ed19f193fc820c091133293b
Aslanchen_TheOne <122560007@163.com>
2017-03-09 15:07:43 +0800

    update

--- app/src/main/java/aslanchen/screensharing/MainActivity.java
+++ app/src/main/java/aslanchen/screensharing/MainActivity.java
@@ -23,8 +23,8 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity implements OnItemClickListener,
-        OnClickListener {
+
+public class MainActivity extends Activity implements OnItemClickListener, OnClickListener {
 
     public static final int DISPLAY_STATE_NOT_CONNECTED = 0;
     public static final int DISPLAY_STATE_CONNECTING = 1;
@@ -32,14 +32,18 @@ public class MainActivity extends Activity implements OnItemClickListener,
 
     private Button bt_back, bt_rush;
     private ListView ls_show;
+
     private List<WifiDisplayPro> listAvilable;
     private List<WifiDisplayPro> listRemember;
     private WifiDisplayAdapter wifiAdapter;
     private int connectIndex = -1;
+
     private DisplayManager mDisplayManager;
+
     private int state = 0;// 0：断开 1：正在连接 2：已经连接
     private Method getWifiDisplayStatus;
     private Object mWifiDisplayStatus;
+
     private Runnable outRunnable;
     private Handler outHandle;
 
@@ -70,8 +74,7 @@ public class MainActivity extends Activity implements OnItemClickListener,
         iss.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
         this.registerReceiver(mBroadcastReceiver, new IntentFilter(iss));
 
-        mDisplayManager = (DisplayManager) this
-                .getSystemService(Context.DISPLAY_SERVICE);
+        mDisplayManager = (DisplayManager) this.getSystemService(Context.DISPLAY_SERVICE);
         listAvilable = new ArrayList<WifiDisplayPro>();
         listRemember = new ArrayList<WifiDisplayPro>();
 
@@ -83,10 +86,10 @@ public class MainActivity extends Activity implements OnItemClickListener,
 
             @Override
             public void run() {
-                Toast.makeText(MainActivity.this, "连接超时,请重启连接设备！",
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "连接超时,请重启连接设备！", Toast.LENGTH_SHORT).show();
             }
         };
+
         startScan();
     }
 
@@ -101,50 +104,38 @@ public class MainActivity extends Activity implements OnItemClickListener,
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position,
-                            long id) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         if (position == connectIndex) {
             // 断开
             try {
-                Method disconnectWifiDisplay = mDisplayManager.getClass()
-                        .getMethod("disconnectWifiDisplay");
+                Method disconnectWifiDisplay = mDisplayManager.getClass().getMethod("disconnectWifiDisplay");
                 disconnectWifiDisplay.invoke(mDisplayManager);
                 connectIndex = -1;
                 state = DISPLAY_STATE_NOT_CONNECTED;
             } catch (NoSuchMethodException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IllegalArgumentException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         } else {
             // 连接
             try {
-                Method connectWifiDisplay = mDisplayManager.getClass()
-                        .getMethod("connectWifiDisplay", String.class);
-                connectWifiDisplay.invoke(mDisplayManager,
-                        listAvilable.get(position).getDeviceAddress());
+                Method connectWifiDisplay = mDisplayManager.getClass().getMethod("connectWifiDisplay", String.class);
+                connectWifiDisplay.invoke(mDisplayManager, listAvilable.get(position).getDeviceAddress());
                 connectIndex = position;
                 state = DISPLAY_STATE_CONNECTING;
                 outHandle.postDelayed(outRunnable, 10 * 1000);// 超时检查
             } catch (NoSuchMethodException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IllegalArgumentException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
@@ -153,28 +144,23 @@ public class MainActivity extends Activity implements OnItemClickListener,
         wifiAdapter.notifyDataSetChanged();
     }
 
+
     private void startScan() {
         try {
             // 反射出扫描方法
-            Method scanWifiDisplays = mDisplayManager.getClass().getMethod(
-                    "scanWifiDisplays");
+            Method scanWifiDisplays = mDisplayManager.getClass().getMethod("scanWifiDisplays");
             // 反射出获取扫描结果方法
-            getWifiDisplayStatus = mDisplayManager.getClass().getMethod(
-                    "getWifiDisplayStatus");
+            getWifiDisplayStatus = mDisplayManager.getClass().getMethod("getWifiDisplayStatus");
 
             // 开始扫描
             scanWifiDisplays.invoke(mDisplayManager);
         } catch (NoSuchMethodException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (InvocationTargetException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -188,11 +174,9 @@ public class MainActivity extends Activity implements OnItemClickListener,
             mWifiDisplayStatus = getWifiDisplayStatus.invoke(mDisplayManager);
 
             // 获取支持的设备值
-            Field mAvailableDisplays = mWifiDisplayStatus.getClass()
-                    .getDeclaredField("mAvailableDisplays");
+            Field mAvailableDisplays = mWifiDisplayStatus.getClass().getDeclaredField("mAvailableDisplays");
             mAvailableDisplays.setAccessible(true);// 设置安全检查，访问私有成员变量必须
-            Object mAvailablefildValue = mAvailableDisplays
-                    .get(mWifiDisplayStatus);
+            Object mAvailablefildValue = mAvailableDisplays.get(mWifiDisplayStatus);
             Object[] osAva = (Object[]) mAvailablefildValue;
 
             for (int i = 0; i < osAva.length; i++) {
@@ -201,11 +185,9 @@ public class MainActivity extends Activity implements OnItemClickListener,
             }
 
             // 获取记住的设备
-            Field mRememberedDisplays = mWifiDisplayStatus.getClass()
-                    .getDeclaredField("mRememberedDisplays");
+            Field mRememberedDisplays = mWifiDisplayStatus.getClass().getDeclaredField("mRememberedDisplays");
             mRememberedDisplays.setAccessible(true);// 设置安全检查，访问私有成员变量必须
-            Object mRememberedfildValue = mRememberedDisplays
-                    .get(mWifiDisplayStatus);
+            Object mRememberedfildValue = mRememberedDisplays.get(mWifiDisplayStatus);
             Object[] osRember = (Object[]) mRememberedfildValue;
 
             for (int i = 0; i < osRember.length; i++) {
@@ -222,16 +204,12 @@ public class MainActivity extends Activity implements OnItemClickListener,
             wifiAdapter.notifyDataSetChanged();
 
         } catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (InvocationTargetException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (NoSuchFieldException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -239,8 +217,7 @@ public class MainActivity extends Activity implements OnItemClickListener,
     private void getActiveDisplayValue(Object[] osAva) {
         try {
             // 获取当前连接屏幕
-            Field mActiveDisplay = mWifiDisplayStatus.getClass()
-                    .getDeclaredField("mActiveDisplay");
+            Field mActiveDisplay = mWifiDisplayStatus.getClass().getDeclaredField("mActiveDisplay");
             mActiveDisplay.setAccessible(true);// 设置安全检查，访问私有成员变量必须
             Object mActiveDisplayValue = mActiveDisplay.get(mWifiDisplayStatus);
             connectIndex = -1;
@@ -253,13 +230,10 @@ public class MainActivity extends Activity implements OnItemClickListener,
                 }
             }
         } catch (NoSuchFieldException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -267,29 +241,22 @@ public class MainActivity extends Activity implements OnItemClickListener,
     private void getActiveDisplayState() {
         // 获取当前连接状态
         try {
-            Field mActiveDisplayState = mWifiDisplayStatus.getClass()
-                    .getDeclaredField("mActiveDisplayState");
+            Field mActiveDisplayState = mWifiDisplayStatus.getClass().getDeclaredField("mActiveDisplayState");
             mActiveDisplayState.setAccessible(true);// 设置安全检查，访问私有成员变量必须
-            Object mActiveDisplayStateValue = mActiveDisplayState
-                    .get(mWifiDisplayStatus);
+            Object mActiveDisplayStateValue = mActiveDisplayState.get(mWifiDisplayStatus);
             state = Integer.valueOf(String.valueOf(mActiveDisplayStateValue));
         } catch (NoSuchFieldException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IllegalAccessException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    private WifiDisplayPro getValue(Object clas) throws NoSuchFieldException,
-            IllegalArgumentException, IllegalAccessException {
+    private WifiDisplayPro getValue(Object clas) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
         WifiDisplayPro wdp = new WifiDisplayPro();
-        String[] names = new String[]{"mDeviceAddress", "mDeviceAlias",
-                "mDeviceName", "mDeviceType", "mGroupCapability"};
+        String[] names = new String[]{"mDeviceAddress", "mDeviceAlias", "mDeviceName", "mDeviceType", "mGroupCapability"};
 
         for (int i = 0; i < names.length; i++) {
             Field field = clas.getClass().getDeclaredField(names[i]);
@@ -330,8 +297,7 @@ public class MainActivity extends Activity implements OnItemClickListener,
                     System.out.println("获取可用设备");
                     getAvivableDrive();
                 }
-            } else if ("android.hardware.display.action.WIFI_DISPLAY_STATUS_CHANGED"
-                    .equals(action) == true) {
+            } else if ("android.hardware.display.action.WIFI_DISPLAY_STATUS_CHANGED".equals(action) == true) {
                 // 连接上，断开会触发这个
                 if (mWifiDisplayStatus != null) {
                     System.out.println("连接或者断开");
